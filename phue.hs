80fe732ee1ae95414de6faea7a965a8eb777abae
meris <meris.bahti@gmail.com>
2014-12-21 20:56:52 +0100

    add example of how to get the state of a light

--- phue.hs
+++ phue.hs
@@ -5,6 +5,7 @@ import Data.Aeson
 import Control.Applicative
 import Data.ByteString.Lazy (pack)
 import Data.ByteString.Internal
+import Data.Map.Strict (Map, fromList, (!), adjust)
 
 data Light = Light 
   { state :: LightState
@@ -14,12 +15,6 @@ instance FromJSON Light where
    parseJSON (Object v) = Light <$>
                           v .: "state"
    parseJSON _          = error "Can't decode into Light - not Object."
-
-
-exJsonLightState' :: String
-exJsonLightState' = "{\"on\":true,\"bri\":174,\"hue\":0,\"sat\":0,\"xy\":[0.4753,0.4676],\"ct\":241,\"alert\":\"none\",\"effect\":\"none\",\"colormode\":\"xy\",\"reachable\":false}"
-exJsonLightState = exJsonLightState'
-cs = Data.ByteString.Lazy.pack . map c2w 
 
 data LightState = LightState 
   { on        :: Bool
@@ -43,7 +38,14 @@ instance FromJSON LightState where
    -- A non-Object value is of the wrong type, so fail.
    parseJSON _          = error "Can't decode into LightState - not Object."
 
-lights :: IO Data.ByteString.Lazy.Internal.ByteString
-lights = simpleHTTP (getRequest $ apiBaseUrl ++ "lights") >>= getResponseBody >>= return .  cs
+cs = Data.ByteString.Lazy.pack . map c2w 
 
+lights     = simpleHTTP (getRequest $ apiBaseUrl ++ "lights") >>= getResponseBody >>= return .  cs
 apiBaseUrl = "http://192.168.2.13/api/1167c7272c58f39f20e271172bbe1f8f/"
+currStatus = lights >>= return . decode :: IO (Maybe (Map String Light))
+{-
+This is how you would get the status for light #1:
+  m = do 
+    x <- currStatus
+    return $ fmap (! "1") x 
+-}
